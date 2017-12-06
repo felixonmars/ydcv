4093b2bb68a838d3531592489d92ffb25224c74c
Felix Yan <felixonmars@archlinux.org>
2017-12-06 17:06:59 +0800

    setup: use scm and setuptools-markdown

--- setup.py
+++ setup.py
@@ -1,24 +1,42 @@
 #!/usr/bin/env python
+import sys
 from setuptools import setup
 
-with open('README.md') as f:
-    long_description = f.read().strip()
+setup_requires = ['setuptools_scm']
+if sys.argv[-1] in ('sdist', 'bdist_wheel'):
+    setup_requires.append('setuptools-markdown')
 
 
 setup(
     name="ydcv",
     version="0.0.1",
     description="YouDao Console Version, a simple wrapper for Youdao API",
-    long_description=long_description,
-    # classifiers=[],
-    # keywords="",
+    long_description_markdown_filename='README.md',
     author="Felix Yan",
     author_email="felixonmars@archlinux.org",
     url="https://github.com/felixonmars/ydcv",
-    # license="",
+    license="GPL",
     package_dir={'': 'src'},
     py_modules=['ydcv'],
     entry_points={
         'console_scripts': ['ydcv=ydcv:main'],
     },
+    setup_requires=setup_requires,
+    use_scm_version=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GPL License",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: SunOS/Solaris",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Utilities",
+    ]
 )
